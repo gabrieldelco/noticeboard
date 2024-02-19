1fa1279201c20d57d3f8f24c7f8ea3c88b8d69ca
gabrieldelco <gabrieldelconti99@gmail.com>
2024-02-19 16:37:32 -0300

    created posts object file

---
+++ model/posts.js
@@ -0,0 +1,34 @@
+module.exports = {
+    posts: [
+        {
+            id: "sidufh",
+            title: "Teste do Mural",
+            description: "Descrição teste"
+        },
+        {
+            id: "aaa",
+            title: "Teste do Mural",
+            description: "Descrição teste"
+        },
+    ],
+
+    getAll() {
+        return this.posts;
+    },
+
+    newPost(title, description) {
+        this.posts.push({ id: generateID(), title, description });
+    },
+
+    deletePost(id) {
+        const index = this.posts.findIndex(p => p.id === id)
+        if (index !== -1) {
+            this.posts.splice(index, 1);
+        }
+    }
+
+}
+
+function generateID() {
+    return Math.random().toString(36).substring(2, 13);
+}
