d85f8b740c2bc1e4241ac473bb02c9d142446fd0
gabrieldelco <gabrieldelconti99@gmail.com>
2024-02-19 20:29:48 -0300

    adding cors options

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const PORT = 3000;
 const express = require('express');
 const path = require('path');
 const apiRoute = require('./routes/api');
-const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
 app.use('/api', apiRoute)
 app.use('/', express.static(path.join(__dirname, "public")));
 
