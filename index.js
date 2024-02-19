1db5eb091f807c8f173e91d5254109f72c89dea0
gabrieldelco <gabrieldelconti99@gmail.com>
2024-02-19 16:39:14 -0300

    added delete feature; implemented object posts methods

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 const PORT = 3000;
 const express = require('express');
+const posts = require('./model/posts')
 
 const app = express();
 
-let posts = [
-    {
-        id: "sidufh",
-        title: "Teste do Mural",
-        description: "Descrição teste"
-    },
-];
-
 app.get("/all", (req, res) => {
 
-    res.json(JSON.stringify(posts));
+    res.json(JSON.stringify(posts.getAll()));
 })
 
 app.post("/new", express.json(), (req, res) => {
-    let id = generateID();
+
     let title = req.body.title;
     let description = req.body.description;
+    posts.newPost(title, description);
 
-    posts.push({ id, title, description });
 
     res.send("Post adicionado");
 })
 
+app.delete("/del", express.json(), (req, res) => {
+    const id = req.query.id
+    posts.deletePost(id);
+
+    res.send("Post apagado");
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}!`)
 })
-
-function generateID() {
-    return Math.random().toString(36).substring(2, 13);
-}
