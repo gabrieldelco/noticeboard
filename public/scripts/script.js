bbed2b8c3e880ec8e40f73869b01377a59099610
gabrieldelco <gabrieldelconti99@gmail.com>
2024-02-19 20:21:46 -0300

    feat: newPost; delete btn

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -13,23 +13,41 @@ function updatePosts() {
 
         posts.forEach((post) => {
             let postElement = `<div id="${post.id}" class="card mb-4">
-                                    <div class="card-header">
+                                    <div class="d-flex justify-content-between card-header">
                                         <h5 class="card-title">${post.title}</h5>
+                                        <button id="${post.id}" type="button" onclick="deletePost()" class="btn btn-sm btn-outline-danger">X</button>
                                     </div>
                                     <div class="card-body">
                                         <div class="card-text">${post.description}</div>
                                     </div>
                                 </div>`
-                postElements+= postElement;
+            postElements += postElement;
         })
-        document.getElementById("posts").innerHTML=postElements;
+        document.getElementById("posts").innerHTML = postElements;
     })
 }
 
 function newPost() {
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("desc").value;
 
+    let post = { title, description };
+
+    const options = {
+        method: "POST",
+        headers: new Headers({ 'content-type': 'application/json' }),
+        body: JSON.stringify(post)
+    }
+    fetch("http://localhost:3000/api/new", options).then(res => {
+        console.log(res);
+        updatePosts();
+        document.getElementById("title").value = "";
+        document.getElementById("desc").value = "";
+
+    })
 }
 
-function deletePost() {
+// function deletePost() {
+//     let id = document.getElementById()
 
-}
+// }
