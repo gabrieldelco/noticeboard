c2045b027cbbcd27ae9ad0d8533e652cf9ec8d33
gabrieldelco <gabrieldelconti99@gmail.com>
2024-02-19 19:44:56 -0300

    front-end script getting posts

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const PORT = 3000;
 const express = require('express');
 const path = require('path');
-const apiRoute = require('./routes/api')
+const apiRoute = require('./routes/api');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use('/api', apiRoute)
 app.use('/', express.static(path.join(__dirname, "public")));
 
